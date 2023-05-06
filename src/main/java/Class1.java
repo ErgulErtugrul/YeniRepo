9f38153716aa3bdd72f9992cdd7c312a377efc8d
ErgulErtugrul <ertugrulergul.7@gmail.com>
2023-05-06 21:53:52 +0300

    v4

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -4,6 +4,10 @@ public class Class1 {
         //Version 2 üzerine çalışmalar...
         System.out.println("v3 kodları...");//v3
         //v4 kısa yoldan commit
+        //v5 kısa yoldan commit
+
+
+
 
 
 
